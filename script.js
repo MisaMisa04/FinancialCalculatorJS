a44fb46cd968048b80db49bb5cd4aa27a103cf4c
dsdest <vlad_zdor_94@mail.ru>
2020-04-27 20:31:43 +0300

    Доделан, но пока запоминает только в localStorage

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const totalMoneyIncome=document.querySelector('.total__money-income'),
         form=document.getElementById('form'),
         operationName=document.querySelector('.operation__name'),
         operationAmount=document.querySelector('.operation__amount');
-
+localStorage.clear();
 let dbOperation=JSON.parse(localStorage.getItem('calc')) || [];
 
 const generateId = () => {
@@ -18,7 +18,7 @@ const renderOperation = (item) => {
     listItem.classList.add(item.amount>=0 ? 'history__item-plus' : 'history__item-minus');
     listItem.innerHTML=`${item.description}
         <span class="history__money">${item.amount} ₽</span>
-        <button class="history_delete">x</button>
+        <button class="history_delete" data-id="${item.id}">x</button>
     `;
     historyList.append(listItem);
 };
@@ -27,25 +27,12 @@ const updateBalance = () => {
     const resultIncome = dbOperation
         .filter((element) => element.amount > 0)
         .reduce((result, element)=>result+element.amount, 0);
-
     totalMoneyIncome.textContent=resultIncome+' ₽';
     const resultSpends = dbOperation
         .filter((element) => element.amount < 0)
         .reduce((result, element)=>result+element.amount, 0);
     totalMoneyExpenses.textContent=resultSpends+' ₽';
     totalBalance.textContent=resultSpends+resultIncome+' ₽';
-};
-
-const deleteOperation = (event) => {
-    const target=event.target;
-    if (target.classList.contains('history_delete'))
-    {
-        console.log(target);
-        dbOperation=dbOperation
-            .filter(operation => operation.id!==target.dataset.id);
-        init();
-    }
-    
 };
 
 const init = () => {
@@ -56,29 +43,40 @@ const init = () => {
 };
 
 const addOperation = (event) => {
+    operationName.style.borderColor='';
+    operationAmount.style.borderColor='';
     event.preventDefault();
     const operationNameValue=operationName.value,
         operationAmountValue=operationAmount.value;
     if (operationNameValue && operationAmountValue)
     {
-        const newOperation =
-        {
+        const operation = {
             id: generateId(),
-            description: operationNameValue, 
-            amount: operationAmountValue
+            description: operationNameValue,
+            amount: +operationAmountValue,
         };
-        dbOperation.push(newOperation);
+        dbOperation.push(operation);
         init();
     }
     else 
     {
         if (!operationNameValue)
-            operationName.style.borderColor = 'red';
-        if (!operationAmountValue)
-            operationAmount.style.borderColor = 'red';
+            operationName.style.borderColor='red';
+        if(!operationAmountValue)
+            operationAmount.style.borderColor='red';
     }
-    operationName.style.borderColor = '';
-    operationAmount.style.borderColor = '';
+    operationName.value='';
+    operationAmount.value='';
+};
+
+const deleteOperation = (event) => {
+    const target=event.target;
+    if (event.target.classList.contains('history_delete'))
+    {
+        dbOperation = dbOperation
+            .filter(operation => operation.id!==target.dataset.id);
+        init();
+    }
 };
 
 form.addEventListener('submit', addOperation);
